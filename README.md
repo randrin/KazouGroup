9efac35713e73ffc7c540bbbf102da6cc8ba8261
Randrin <nzeukang.randrin@accenture.com>
2019-11-21 09:26:29 +0100

    Release dist/v1.2.3

--- README.md
+++
@@ -1,2 +0,0 @@
-# KazouGroup
-Groupe qui rassemble une variété de services pour le public
--- assets/kazoukit/scripts/kazoukit.js
+++
@@ -1,2 +0,0 @@
-import kazouUiKit from "./commons/kazouUiKit";
-import validationForm from "./globals/validationForm";
